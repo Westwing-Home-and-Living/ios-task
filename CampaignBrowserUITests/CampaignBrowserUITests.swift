da2ab9de1e8604c061f729eff96438bd3271cbf6
Alfonso Calderón Martínez <alfocal@icloud.com>
2021-05-20 18:19:40 +0200

    add a UITest to ensure campaign images validate design requirements

--- CampaignBrowserUITests/CampaignBrowserUITests.swift
+++ CampaignBrowserUITests/CampaignBrowserUITests.swift
@@ -3,16 +3,26 @@ import XCTest
 class CampaignBrowserUITests: XCTestCase {
 
     var application: XCUIApplication!
+    let imageOriginX: CGFloat = 0
+    let imageRatio: CGFloat = 4 / 3
 
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
         application = XCUIApplication()
+        application.launch()
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCampaignImages() {
+        let firstImage = application.images.element(boundBy: 0)
+        XCTAssert(firstImage.exists, "There should be an image in the campaign")
+        let imageSize = firstImage.frame.size
+        // using UIScreen is not a valid way of getting the device screen size in testing
+        let screenSize = application.windows.element(boundBy: 0).frame.size
+        XCTAssert(firstImage.frame.origin.x == imageOriginX, "Images should start at the beginning of the screen")
+        XCTAssert(imageSize.width == screenSize.width, "Images should take all available width")
+        XCTAssert((imageSize.height * imageRatio).rounded() == imageSize.width, "Images proportion should be 4:3")
+
     }
 
 }
