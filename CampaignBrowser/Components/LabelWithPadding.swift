871555b860ca093cdf5fb4bbead5d20e8bce7f8b
Alfonso Calderón Martínez <alfocal@icloud.com>
2021-05-20 23:21:52 +0200

    added override to LabelWithPadding to ensure the size calculation is done properly

--- CampaignBrowser/Components/LabelWithPadding.swift
+++ CampaignBrowser/Components/LabelWithPadding.swift
@@ -18,6 +18,14 @@ class LabelWithPadding: UILabel {
                                                        right: horizontalPadding)))
     }
 
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insettedBounds = bounds.inset(by: UIEdgeInsets(top: verticalPadding,
+                                                 left: horizontalPadding,
+                                                 bottom: verticalPadding,
+                                                 right: horizontalPadding))
+        return super.textRect(forBounds: insettedBounds, limitedToNumberOfLines: 0)
+    }
+
     override var intrinsicContentSize: CGSize {
         let originalSize = super.intrinsicContentSize
         return CGSize(width: originalSize.width + horizontalPadding * 2,
